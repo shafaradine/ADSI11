67c16e5b6eebe53c24d44ff33f32cb210b5f6e24
shafaradine <shafaradina@student.ub.ac.id>
2022-12-17 21:17:15 +0700

    update

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.45.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Ambis Yuk!</title>
+</head>
+
+<body>
+    <!--Navbar-->
+    <div class="navbar bg-base-100 fixed border-b-2 border-slate-300">
+        <div class="flex-1">
+            <a class="btn btn-ghost normal-case text-xl ml-5">Ambis Yuk!</a>
+        </div>
+        <div class="flex-none pr-10">
+            <ul class="menu menu-horizontal px-1 text-blue-900 font-medium">
+                <li><a>ABOUT US</a></li>
+                <li><a>LOGIN</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <!--CONTENT-->
+    <div class="container px-10 py-20">
+        <div class="grid grid-cols-2 justify-items-center">
+            <div class="object-center">
+                <img style="width:420px" src="https://img.freepik.com/free-vector/boy-student-sitting-stack-books-with-laptop-flat-icon-illustration_1284-64037.jpg?w=740&t=st=1671309483~exp=1671310083~hmac=c350ea4db30889c8380361de2c50490e3368f5930d68e7cf404e8212143fca81" />
+            </div>
+            <div class="m-auto text-center">
+                <p class="text-3xl font-bold pb-5">Dapatkan Informasi <br>
+                    tentang Pendidikan dan Pekerjaan
+                </p>
+            </div>
+            <div class="m-auto text-center">
+                <p class="text-3xl font-bold pb-5">Lebih dari 3000+++ <br>
+                    lowongan magang terbuka
+                </p>
+            </div>
+            <div class="object-center">
+                <img style="width:420px" src="https://img.freepik.com/free-vector/boy-student-sitting-stack-books-with-laptop-flat-icon-illustration_1284-64037.jpg?w=740&t=st=1671309483~exp=1671310083~hmac=c350ea4db30889c8380361de2c50490e3368f5930d68e7cf404e8212143fca81" />
+            </div>
+            <div class="object-center">
+                <img style="width:420px" src="https://img.freepik.com/free-vector/boy-student-sitting-stack-books-with-laptop-flat-icon-illustration_1284-64037.jpg?w=740&t=st=1671309483~exp=1671310083~hmac=c350ea4db30889c8380361de2c50490e3368f5930d68e7cf404e8212143fca81" />
+            </div>
+            <div class="m-auto text-center">
+                <p class="text-3xl font-bold pb-5">Lebih dari 5000+++ <br>
+                    kesempatan beasiswa tersedia
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
